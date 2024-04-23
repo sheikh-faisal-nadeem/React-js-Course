9655547cb63f08c949a64339f5e505c78240218e
Sheikh Faisal Nadeem <nfaisal679@gmail.com>
2024-04-24 03:21:05 +0500

    Code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code With Sheikh
-        </a>
-      </header>
-    </div>
+        <div>
+            Code With Sheikh
+        </div>
   );
 }
 
