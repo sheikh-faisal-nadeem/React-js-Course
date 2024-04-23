edac10a66473afe44bfb04cc2b75781d241a13bf
Sheikh Faisal Nadeem <nfaisal679@gmail.com>
2024-04-24 03:41:59 +0500

    header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header' >
+            <div  className='link' >Home</div>
+            <div className='link' >About</div>
+            <div className='link'>Contact</div>
+            <div className='link' >Blog</div>
+    </div>
+  )
+}
+
+export default Header
+
