713df8dacb17799e0199775e6cc96bbfe468c2a8
Sheikh Faisal Nadeem <nfaisal679@gmail.com>
2024-05-01 01:49:50 +0500

    Form handling

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="Form">
+      <h2>Form Handling.....!!!!!!</h2>
+
+   
+    </div>
+  );
+};
+
+export default App;
